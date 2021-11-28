11f052464143b126c7b51ac7dd0b5ffff701325d
Masonym <nosamleitch@gmail.com>
2021-11-28 13:21:49 -0800

    Added gitignore and some comments

--- Data/CommanderContext.cs
+++ Data/CommanderContext.cs
@@ -3,13 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Commander.Data
 {
+    //inheriting DbContext from EFCore
     public class CommanderContext : DbContext
     {
+
         public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
         {
 
         }
 
+        //representing our Command objects to our database as a DbSet
+        //mapping is done here for objects in the Models namespace. more objects there means more mapping to be done here
         public DbSet<Command> Commands { get; set; }
     }
 }
